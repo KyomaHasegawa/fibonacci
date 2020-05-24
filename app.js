9a8fb82446fe5e60ff46e82a9fd29c36b2939f75
KyomaHasegawa <mou.61.2424@gmail.com>
2020-05-24 14:47:27 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 'use strict';
+var fibonacci = new Map();
+const length = 40;
+//フィボナッチ数列作成
+for (let i=0;i<=length;i++){
+    fibonacci.set(i,fib(i));
+    console.log(fibonacci.get(i));
+}
+
+//フィボナッチ数列作成関数
 function fib(n){
     if(n===0){
         return 0;
     }else if(n===1){
         return 1;
     }
-    return fib(n-1) + fib(n-2);
-}
-
-const length = 40;
-for (let i =0;i <= length; i++){
-    console.log(fib(i));
+    return fibonacci.get(n-1) + fibonacci.get(n-2);
 }
